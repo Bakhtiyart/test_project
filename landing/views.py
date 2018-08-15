b3fe222ccf427b1902029d124fc819a8492061df
Bakhtiyar Tayirov <bakhtiyarkhozha@gmail.com>
2018-08-15 11:36:04 +0500

    init project

---
+++ landing/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+def landing(request):
+    form = SubscriberForm(request.POST or None)
+    return render(request, 'landing/landing.html', locals())
